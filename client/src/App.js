88f73e237c22d8037d3994dd30c6e8067dd5ce98
MasonBeynon <beynomas000@gmail.com>
2025-04-06 15:35:44 -0500

    Initial code

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Habit Tracker</h1>
+      <p>Welcome to your streak tracker!</p>
+    </div>
+  );
+}
+
+export default App;
